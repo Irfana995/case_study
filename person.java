51d972bdc0c91a0af788e62e51e578d5e055eba0
Irfana995 <58158728+Irfana995@users.noreply.github.com>
2019-12-02 10:58:28 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,36 @@
+package casestudy;
+
+public class person {
+	int id;
+	String name,address,mob,mail,prooft,proofno;
+	void register(int id,String name,String address,String mob,String mail,String prooft,String proofno)
+	{
+		this.id=id;
+		this.name=name;
+		this.address=address;
+		this.mob=mob;
+		this.mail=mail;
+		this.prooft=prooft;
+		this.proofno=proofno;
+	}
+	void updatemail(String mail)
+	{
+		this.mail=mail;
+	}
+	void display1()
+	{
+		System.out.println("Your details are as follows\n.Name:"+this.name
+				+"\n.Address: "+this.address+"\n.Contact Number: "+this.mob
+				+"\n.Mail ID: "+this.mail+"\n.Proof type: "+this.prooft
+				+"\n.Proof id: "+this.id+"\n--------------------------------------");
+	}
+	public void viewcustomer() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
